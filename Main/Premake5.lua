0a48f293c65dc3613ed5cf2665def697e61fd619
d4nze <daniiiiila123@gmail.com>
2025-01-28 06:23:16 +0300

    Added SFML 3.0.0 API

--- Main/Premake5.lua
+++ Main/Premake5.lua
@@ -21,22 +21,65 @@ project "Main"
     }
     
     includedirs {
-		"Source",
-		"../Core/Source",
-		"../ScriptingEnvironment/Source"
+		"Source"
 	}
 	libdirs {}
     links {}
-	postbuildcommands {
-		---copyFile("%{wks.location}ScriptingEnvironment\\Build\\bin\\%{outputdir}\\ScriptingEnvironment.dll")
-	}
 	
 	filter "platforms:Win32"
 		architecture "x86"
         defines "WIN32"
+		includedirs { 
+			"../Dependencies/SFML/x86/include"
+		}
+		libdirs { 
+			"../Dependencies/SFML/x86/lib"
+		}
+		links {
+			"sfml-audio.lib",
+			"sfml-graphics.lib",
+			"sfml-network.lib",
+			"sfml-system.lib",
+			"sfml-window.lib"
+		}
+		postbuildcommands {
+			copyFile("%{wks.location}Dependencies\\SFML\\x86\\bin\\sfml-audio-3.dll"),
+			copyFile("%{wks.location}Dependencies\\SFML\\x86\\bin\\sfml-graphics-3.dll"),
+			copyFile("%{wks.location}Dependencies\\SFML\\x86\\bin\\sfml-network-3.dll"),
+			copyFile("%{wks.location}Dependencies\\SFML\\x86\\bin\\sfml-system-3.dll"),
+			copyFile("%{wks.location}Dependencies\\SFML\\x86\\bin\\sfml-window-3.dll"),
+			copyFile("%{wks.location}Dependencies\\SFML\\x86\\bin\\sfml-graphics-d-3.dll"),
+			copyFile("%{wks.location}Dependencies\\SFML\\x86\\bin\\sfml-network-d-3.dll"),
+			copyFile("%{wks.location}Dependencies\\SFML\\x86\\bin\\sfml-system-d-3.dll"),
+			copyFile("%{wks.location}Dependencies\\SFML\\x86\\bin\\sfml-window-d-3.dll")
+		}
 	filter "platforms:Win64"
 		architecture "x86_64"
-        defines "WIN64"	
+        defines "WIN64"
+		includedirs { 
+			"../Dependencies/SFML/x64/include"
+		}
+		libdirs { 
+			"../Dependencies/SFML/x64/lib"
+		}
+		links {
+			"sfml-audio.lib",
+			"sfml-graphics.lib",
+			"sfml-network.lib",
+			"sfml-system.lib",
+			"sfml-window.lib"
+		}
+		postbuildcommands {
+			copyFile("%{wks.location}Dependencies\\SFML\\x64\\bin\\sfml-audio-3.dll"),
+			copyFile("%{wks.location}Dependencies\\SFML\\x64\\bin\\sfml-graphics-3.dll"),
+			copyFile("%{wks.location}Dependencies\\SFML\\x64\\bin\\sfml-network-3.dll"),
+			copyFile("%{wks.location}Dependencies\\SFML\\x64\\bin\\sfml-system-3.dll"),
+			copyFile("%{wks.location}Dependencies\\SFML\\x64\\bin\\sfml-window-3.dll"),
+			copyFile("%{wks.location}Dependencies\\SFML\\x64\\bin\\sfml-graphics-d-3.dll"),
+			copyFile("%{wks.location}Dependencies\\SFML\\x64\\bin\\sfml-network-d-3.dll"),
+			copyFile("%{wks.location}Dependencies\\SFML\\x64\\bin\\sfml-system-d-3.dll"),
+			copyFile("%{wks.location}Dependencies\\SFML\\x64\\bin\\sfml-window-d-3.dll")
+		}
 	filter "configurations:Debug"
         symbols "On"
         defines "DEBUG"
--- Main/Source/Main.cpp
+++ Main/Source/Main.cpp
@@ -1,8 +1,28 @@
 #include <iostream>
+#include <SFML/Graphics.hpp>
+#include <SFML/Window/Event.hpp>
 
-int main()
+std::int32_t main()
 {
-    std::cout << "Yo\n";
-    system("pause");
+    sf::RenderWindow window(sf::VideoMode(sf::Vector2u(640, 480)), "SFML Window");
+    sf::RectangleShape rectangle(sf::Vector2f(100.f, 100.f));
+    rectangle.setPosition(sf::Vector2f(320.f, 240.f) - rectangle.getSize() / 2.f);
+    rectangle.setFillColor(sf::Color::Green);
+
+    while (window.isOpen())
+    {
+        while (const std::optional<sf::Event> event = window.pollEvent())
+        {
+            if (event->is<sf::Event::Closed>())
+            {
+                window.close();
+            }
+        }
+
+        window.clear();
+        window.draw(rectangle);
+        window.display();
+    }
+
     return 0;
 }
