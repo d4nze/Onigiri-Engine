854095484d0fbc893a67852fb5ea7e69f14d0a1f
d4nze <daniiiiila123@gmail.com>
2025-02-08 21:49:08 +0300

    Added basic GUI.

--- Launcher/Source/ProjectCreation.cpp
+++ Launcher/Source/ProjectCreation.cpp
@@ -1,5 +1,28 @@
 #include "ProjectCreation.hpp"
+#include "LauncherGUI.hpp"
+#include "ProjectSelection.hpp"
+
+#include <imgui.h>
 
 ProjectCreation::ProjectCreation(LauncherGUI& launcherGUI) : m_launcherGUI(launcherGUI) {}
 
-void ProjectCreation::update() {}
+void ProjectCreation::update()
+{
+	ImGui::Dummy(ImVec2(0, ImGui::GetContentRegionAvail().y - 25));
+
+    float buttonWidth = 80.0f;
+    float spacing = 10.0f;
+    float windowWidth = ImGui::GetWindowWidth();
+    float totalButtonsWidth = buttonWidth * 2 + spacing;
+
+    if (ImGui::Button("Cancel", ImVec2(buttonWidth, 0)))
+    {
+        m_launcherGUI.getStateManager().setState<ProjectSelection>();
+    }
+    ImGui::SameLine(windowWidth - totalButtonsWidth - ImGui::GetStyle().ItemSpacing.x);
+    ImGui::BeginDisabled();
+    if (ImGui::Button("Back", ImVec2(buttonWidth, 0)));
+    ImGui::EndDisabled();
+    ImGui::SameLine();
+    if (ImGui::Button("Next", ImVec2(buttonWidth, 0)));
+}
