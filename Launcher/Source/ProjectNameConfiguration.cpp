9c40c3daa41e240e12de20237d06e56a10e4700d
d4nze <daniiiiila123@gmail.com>
2025-03-04 03:22:19 +0300

    Added error checks.

--- Launcher/Source/ProjectNameConfiguration.cpp
+++ Launcher/Source/ProjectNameConfiguration.cpp
@@ -1,7 +1,7 @@
 #include "ProjectNameConfiguration.hpp"
 #include "ProjectCreation.hpp"
+#include "Application/LetterChecker.hpp"
 
-#include <cstdint>
 #include <imgui.h>
 
 ProjectNameConfiguration::ProjectNameConfiguration(ProjectCreation& projectCreation)
@@ -13,8 +13,10 @@ ProjectNameConfiguration::ProjectNameConfiguration(ProjectCreation& projectCreat
 void ProjectNameConfiguration::update()
 {
 	ImGui::TextUnformatted("Enter project's name");
-	ImGui::InputText("##Project Name Input", m_nameBuffer, m_bufferSize);
-	updateErrorLog();
+	if (ImGui::InputText("##Project Name Input", m_nameBuffer, m_bufferSize))
+	{
+		updateErrorLog();
+	}
 	if (!m_error.empty())
 	{
 		ImGui::PushStyleColor(ImGuiCol_Text, ImVec4(1.f, 0.f, 0.f, 1.f));
@@ -30,7 +32,43 @@ void ProjectNameConfiguration::reset()
 
 void ProjectNameConfiguration::updateErrorLog()
 {
-	// TODO: Check for naming errors
+	m_error = "";
+	const char fisrtLetter = m_nameBuffer[0];
+	if (fisrtLetter == '\0')
+	{
+		m_error = "Name is not writen";
+		return;
+	}
+	if (fisrtLetter != '_' && !LetterChecker::isLatin(fisrtLetter))
+	{
+		if (!LetterChecker::isNumerical(fisrtLetter))
+		{
+			m_error = "Name must consist only of lating symbols, numbers and '_'";
+			return;
+		}
+		m_error = "Name must start with latin symbol or '_'";
+		return;
+	}
+	for (std::int32_t i = 0; i < m_bufferSize; i++)
+	{
+		const char letter = m_nameBuffer[i];
+		if (letter == '\0')
+		{
+			return;
+		}
+		if (letter == ' ')
+		{
+			m_error = "Name cannot have spaces";
+			return;
+		}
+		if (letter != '_' &&
+			!LetterChecker::isLatin(letter) &&
+			!LetterChecker::isNumerical(letter))
+		{
+			m_error = "Name must consist only of lating symbols, numbers and '_'";
+			return;
+		}
+	}
 }
 
 std::string ProjectNameConfiguration::getName()
