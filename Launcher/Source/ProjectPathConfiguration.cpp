09157e01f6f1bbd91e59ad3f30e4a82b879e6ff8
d4nze <daniiiiila123@gmail.com>
2025-03-08 10:43:34 +0300

    Added project path error handling.

--- Launcher/Source/ProjectPathConfiguration.cpp
+++ Launcher/Source/ProjectPathConfiguration.cpp
@@ -6,8 +6,15 @@
 
 #include <imgui.h>
 
+const char* ProjectPathConfiguration::ErrorVariation::noError = "";
+const char* ProjectPathConfiguration::ErrorVariation::empty = "Please select project folder";
+const char* ProjectPathConfiguration::ErrorVariation::relativePath = "Path must be absolute";
+const char* ProjectPathConfiguration::ErrorVariation::projectExists = "There is an already exiting project with this name";
+const char* ProjectPathConfiguration::ErrorVariation::invalidPath = "Invalid path";
+
 ProjectPathConfiguration::ProjectPathConfiguration(ProjectCreation& projectCreation)
 	: m_projectCreation(projectCreation)
+	, m_error(ErrorVariation::noError)
 	, m_bufferSize(256)
 	, m_pathBuffer("\0")
 {
@@ -17,7 +24,10 @@ ProjectPathConfiguration::ProjectPathConfiguration(ProjectCreation& projectCreat
 void ProjectPathConfiguration::update()
 {
 	ImGui::TextUnformatted("Choose folder for project");
-	ImGui::InputTextWithHint("##Project Name Input", "Path here...", m_pathBuffer, m_bufferSize);
+	if (ImGui::InputTextWithHint("##Project Name Input", "Path here...", m_pathBuffer, m_bufferSize))
+	{
+		updateErrorLog();
+	}
 	ImGui::SameLine();
 	if (ImGui::Button("Browse", ImVec2(80, 0)))
 	{
@@ -25,8 +35,15 @@ void ProjectPathConfiguration::update()
 		if (!path.empty())
 		{
 			std::string sPath = path.string();
-			std::strncpy(m_pathBuffer, sPath.c_str(), sPath.size());
+			std::strncpy(m_pathBuffer, sPath.c_str(), m_bufferSize);
+			updateErrorLog();
 		}
+	}
+	if (m_error != ErrorVariation::noError)
+	{
+		ImGui::PushStyleColor(ImGuiCol_Text, ImVec4(1.f, 0.f, 0.f, 1.f));
+		ImGui::Text(m_error.c_str());
+		ImGui::PopStyleColor();
 	}
 }
 
@@ -37,14 +54,40 @@ void ProjectPathConfiguration::reset()
 	{
 		Project lastProject = *projects.begin();
 		std::strncpy(m_pathBuffer, lastProject.getPath().c_str(), m_bufferSize);
+		m_error = "";
 	}
 	else
 	{
 		std::strncpy(m_pathBuffer, "\0", m_bufferSize);
+		m_error = "Please select";
 	}
+}
+
+void ProjectPathConfiguration::updateErrorLog()
+{
+	m_error = ErrorVariation::noError;
+	std::filesystem::path path = m_pathBuffer;
+	if (m_pathBuffer[0] == '\0')
+	{
+		m_error = ErrorVariation::empty;
+	}
+	else if (path.is_relative())
+	{
+		m_error = ErrorVariation::relativePath;
+	}
+	else if (!std::filesystem::exists(path) && !std::filesystem::is_directory(path))
+	{
+		m_error = ErrorVariation::invalidPath;
+	}
+	// else if (std::filesystem::exists(m_projectCreation.getName())) { ... }
 }
 
 std::string ProjectPathConfiguration::getName()
 {
 	return std::string(m_pathBuffer);
+}
+
+bool ProjectPathConfiguration::hasError() const
+{
+	return m_error != ErrorVariation::noError;
 }
--- Launcher/Source/ProjectPathConfiguration.hpp
+++ Launcher/Source/ProjectPathConfiguration.hpp
@@ -11,11 +11,25 @@ public:
 public:
 	void update();
 	void reset();
+	void updateErrorLog();
 
 	std::string getName();
+	bool hasError() const;
+
+private:
+	class ErrorVariation
+	{
+	public:
+		static const char* noError;
+		static const char* empty;
+		static const char* relativePath;
+		static const char* projectExists;
+		static const char* invalidPath;
+	};
 
 private:
 	ProjectCreation& m_projectCreation;
+	std::string m_error;
 	const std::int32_t m_bufferSize;
 	char m_pathBuffer[256];
 };
