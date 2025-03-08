aa2a2b75edf654c0f09082ef746ae3d1ebb9966e
d4nze <daniiiiila123@gmail.com>
2025-03-08 23:07:07 +0300

    Added basic interface.

--- Launcher/Source/ProjectImporting.cpp
+++ Launcher/Source/ProjectImporting.cpp
@@ -1,8 +1,32 @@
 #include "ProjectImporting.hpp"
+#include "LauncherGUI.hpp"
+
+#include <imgui.h>
 
 ProjectImporting::ProjectImporting(LauncherGUI& launcherGUI) : m_gui(launcherGUI) {}
 
-void ProjectImporting::update() {}
+void ProjectImporting::update()
+{
+    ImGui::BeginChild("Project List", ImVec2(0, ImGui::GetContentRegionAvail().y - 25), true);
+    ImGui::BeginDisabled();
+    ImGui::Text("Importing is not yet supported");
+    ImGui::EndDisabled();
+    ImGui::EndChild();
+
+    float buttonWidth = 80.0f;
+    float spacing = 10.0f;
+    float windowWidth = ImGui::GetWindowWidth();
+    float totalButtonsWidth = buttonWidth * 2 + spacing;
+
+    if (ImGui::Button("Cancel", ImVec2(buttonWidth, 0)))
+    {
+        m_gui.getStateManager().setState<ProjectSelection>();
+    }
+    ImGui::SameLine(windowWidth - buttonWidth - ImGui::GetStyle().ItemSpacing.x);
+    ImGui::BeginDisabled();
+    if (ImGui::Button("Import", ImVec2(buttonWidth, 0))) {}
+    ImGui::EndDisabled();
+}
 
 void ProjectImporting::reset() {}
 
